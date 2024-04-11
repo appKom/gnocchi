180da8f6f813402e53ed5391fea8e9dd821dbd30
Aksel Fosaas <akselsf@stud.ntnu.no>
2024-04-11 16:00:13 +0200

    Add tailwind

---
+++ src/components/universal/Navbar.tsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <div className="bg-[#2e6e53] flex "> 
+        <div className="">
+            <img src="resources/logo/online-logo-white.png"></img>
+        </div>
+    </div>
+  );
+}
+
+export default Navbar;
