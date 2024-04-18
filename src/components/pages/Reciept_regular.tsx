e92d92c77d2e47c4b24eaec2ae8de1c3c381f222
linnerudsander <sandesl@stud.ntnu.no>
2024-04-18 14:55:19 +0200

    header

--- src/components/pages/Reciept_regular.tsx
+++ src/components/pages/Reciept_regular.tsx
@@ -37,8 +37,8 @@ const ReceiptRegular = () => {
                         <input id="default-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
                         <label htmlFor="default-checkbox" className="ms-2 text-sm font-medium text-white dark:text-gray-300">Jeg samtykker til Online Bankom sine Terms and conditions</label>
                     </div>
-                    
                 </div>
+                <h2 className="text-lg md:text-2xl mt-4 mb-3">Kvitteringsinformasjon</h2>
         </div>
     );
 };
