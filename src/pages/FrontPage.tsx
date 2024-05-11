0703f0b38bba51cb76c9df88b55c74098a17851a
Adele Strysse <axstryss@stud.ntnu.no>
2024-05-11 14:57:58 +0200

    frontpage

--- src/pages/FrontPage.tsx
+++ src/pages/FrontPage.tsx
@@ -1,6 +1,9 @@
+import FrontInfo from "../components/FrontInfo"
 import Topbar from "../components/TopBar"
+
 export default function frontPage() {
     return (<body>
     <Topbar/>
+    <FrontInfo/>
     </body>)
 }
