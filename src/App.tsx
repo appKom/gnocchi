5b90f1ce2b7a8bc1a887dbd6eb9990c97a153125
johngoth <johngothe@gmail.com>
2024-10-18 13:21:39 +0200

    Application page added

--- src/App.tsx
+++ src/App.tsx
@@ -6,49 +6,58 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/universal/Home";
 import Authcallback from "./components/authentication/Authcallback";
 import LoginPage from "./pages/LoginPage";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ReceiptPage from "./pages/ReceiptPage";
 import AdminMainPage from "./pages/admin/AdminMainPage";
 import AdminReceiptPage from "./pages/admin/AdminReceiptPage";
 import AdminEconomicRequestPage from "./pages/admin/AdminEconomicRequestPage";
-
-
+import ApplicationPage from "./pages/ApplicationPage";
 
 function App() {
-
   const { isAuthenticated } = useAuth0();
-  const queryClient = new QueryClient()
+  const queryClient = new QueryClient();
 
   const isAdmin = (): Boolean => {
-    return localStorage.getItem('onlineauth0login') != null ? JSON.parse(localStorage.getItem('onlineauth0login')!).isadmin : false;
-  }
+    return localStorage.getItem("onlineauth0login") != null
+      ? JSON.parse(localStorage.getItem("onlineauth0login")!).isadmin
+      : false;
+  };
 
   return (
     <Router>
       <QueryClientProvider client={queryClient}>
-      
-      <div className="App bg-[#2e6e53]">
-      <Navbar />
-        {isAuthenticated ?
-          <Routes>
-            <Route path="/" element={<Home />} /> :
-            <Route path="/authentication/callback" element={<Authcallback />} />
-            <Route path="/kvittering" element={<ReceiptPage />} />
-          </Routes>
-          :
-          <Routes>
-            <Route path="/*" element={<LoginPage />} />
-            <Route path="/authentication/callback" element={<Authcallback />} />
-          </Routes>
-        }
-        {isAuthenticated && isAdmin() &&
-        <Routes>
-          <Route path="/admin/" element={<AdminMainPage />} />
-          <Route path="/admin/kvittering" element={<AdminReceiptPage />} />
-          <Route path="/admin/soknad" element={<AdminEconomicRequestPage />} />
-        </Routes>
-        }
-      </div>
+        <div className="App bg-[#2e6e53]">
+          <Navbar />
+          {isAuthenticated ? (
+            <Routes>
+              <Route path="/" element={<Home />} /> :
+              <Route
+                path="/authentication/callback"
+                element={<Authcallback />}
+              />
+              <Route path="/kvittering" element={<ReceiptPage />} />
+              <Route path="/soknad" element={<ApplicationPage />} />
+            </Routes>
+          ) : (
+            <Routes>
+              <Route path="/*" element={<LoginPage />} />
+              <Route
+                path="/authentication/callback"
+                element={<Authcallback />}
+              />
+            </Routes>
+          )}
+          {isAuthenticated && isAdmin() && (
+            <Routes>
+              <Route path="/admin/" element={<AdminMainPage />} />
+              <Route path="/admin/kvittering" element={<AdminReceiptPage />} />
+              <Route
+                path="/admin/soknad"
+                element={<AdminEconomicRequestPage />}
+              />
+            </Routes>
+          )}
+        </div>
       </QueryClientProvider>
     </Router>
   );
