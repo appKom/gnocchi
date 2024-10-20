648fa7a04e1d5750ff1eec7d250495022e4c8e8b
Emily Louise Malcolmson <emilylm@stud.ntnu.no>
2024-10-20 15:09:17 +0200

    added elements to frontpage

--- src/components/universal/Button.tsx
+++ src/components/universal/Button.tsx
@@ -2,7 +2,7 @@
 
 interface Props {
   title: string;
-  color: "blue" | "white" | "orange";
+  color: "blue" | "white" | "orange" | "green";
   size?: "small";
   icon?: React.ReactNode;
   onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
@@ -10,8 +10,17 @@ interface Props {
 }
 
 const Button = (props: Props) => {
-  let colorClasses = "";
   let sizeClasses = "";
+
+  const colorClassesMap = {
+    green: "bg-green-950 text-white",
+    blue: "bg-blue-300",
+    orange: "bg-orange-300",
+    white: "bg-white text-black",
+  };
+  
+  // Use the `props.color` to dynamically get the class, with a fallback if needed.
+  const colorClasses = colorClassesMap[props.color] || "";
 
   if (props.size === "small") {
     sizeClasses = "px-5 py-2.5 text-sm";
@@ -20,7 +29,7 @@ const Button = (props: Props) => {
   }
 
   const className = `flex rounded-[15px] items-center justify-center relativ p-2  h-[40px] bg-white justify-self-end relative z-20 font-medium text-center justify-center transition-all rounded-lg shadow-sm focus:ring focus:ring-primary-200 inline-flex items-center gap-1.5 ${colorClasses} ${sizeClasses}`;
-
+  
   if (props.href) {
     return (
         <a href={props.href} className={className}>
--- src/pages/FrontPage.tsx
+++ src/pages/FrontPage.tsx
@@ -1,60 +1,33 @@
 import FrontInfo from "../components/FrontInfo"
 import Topbar from "../components/TopBar"
 import Bottombar from "../components/BottomBar"
+import Button from "../components/universal/Button"
+import happy from "../components/happy.png"
 export default function frontPage(){
     return (<body className="bg-[#2E6E53] flex flex-col space-y-4">
     <Topbar/>
     <FrontInfo/>
     <Bottombar/>
-    <header className="p-10 text-white text-2xl font-bold z-30">Velkommen</header>
-    <p className="text-black text-5xl z-30">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-         nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
-         in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-         Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-         anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-         nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
-         in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-         Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-         anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-         nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
-         in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-         Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-         anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-         nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
-         in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-         Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-         anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-         nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
-         in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-         Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-         anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-         nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
-         in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-         Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-         anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-         nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
-         in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-         Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-         anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-         nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
-         in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-         Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-         anim id est laborum.</p>
+    <div className="z-30">
+        <header className="w-1/2 text-white text-5xl font-bold z-30 px-10">Autobank</header>
+        <div className="flex flex-row items-center space-x-4 mx-10">
+        <div className="w-1/2">
+            <p className="text-white px-20 text-left text-xl">
+            Trenger du økonomsik støtte til diverse arrangement?
+            Autobank er et hjelpemiddel for studenter i Online som ønsker å søke om økonomisk støtte eller sende inn kvitteringer for personlige utlegg.
+            Onlines økonomiansvarlige vil behandle dine henvendelser fortløpende. Følg med i din profil for å se statusoppdateringer på dine saker.
+            </p>
+        </div>
+
+        <div className="w-1/2">
+        <img src={happy} alt="" className="money-honey px-20"/>
+        </div>
+        </div>
+        <div className="flex flex-row items-center space-x-10 w-1/2 px-20 mx-10">
+            <Button title={"Logg inn->"} color={"green"}></Button>
+            <Button title={"FAQ"} color={"white"} href="/faq"></Button>
+            <Button title={"Kontakt"} color={"white"}></Button>
+        </div>
+    </div>
     </body>)
 }
