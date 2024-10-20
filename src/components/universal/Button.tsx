e7c14af91c45845ae3547b0d53f9ee729a4e1493
Emily Louise Malcolmson <emilylm@stud.ntnu.no>
2024-10-20 16:46:03 +0200

    fixed styling and button

--- src/components/universal/Button.tsx
+++ src/components/universal/Button.tsx
@@ -20,15 +20,15 @@ const Button = (props: Props) => {
   };
   
   // Use the `props.color` to dynamically get the class, with a fallback if needed.
-  const colorClasses = colorClassesMap[props.color] || "";
+  let colorClasses = colorClassesMap[props.color] || "";
 
   if (props.size === "small") {
-    sizeClasses = "px-5 py-2.5 text-sm";
+    sizeClasses = "w-[100px] h-[40px] px-5 py-2.5 text-sm";
   } else {
-    sizeClasses = "px-6 py-3";
+    sizeClasses = "w-[140px] h-[50px] px-6 py-3";
   }
 
-  const className = `flex rounded-[15px] items-center justify-center relativ p-2  h-[40px] bg-white justify-self-end relative z-20 font-medium text-center justify-center transition-all rounded-lg shadow-sm focus:ring focus:ring-primary-200 inline-flex items-center gap-1.5 ${colorClasses} ${sizeClasses}`;
+  const className = `flex rounded-[15px] items-center justify-center relativ p-2  h-[40px]  justify-self-end relative z-20 font-medium text-center justify-center transition-all rounded-lg shadow-sm focus:ring focus:ring-primary-200 inline-flex items-center gap-1.5 ${colorClasses} ${sizeClasses}`;
   
   if (props.href) {
     return (
