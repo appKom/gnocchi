d6c94bfe60be4fe573b49e6eed2fd463ec1939ab
Adele Strysse <axstryss@stud.ntnu.no>
2025-01-19 16:16:20 +0100

    fix: :bug: Fix committees on ProfileCard
    Fix baseAPI.tsx to only fetch committees the user is a part of, and update ProfileCard.tsx according to the update

--- src/api/baseAPI.tsx
+++ src/api/baseAPI.tsx
@@ -3,10 +3,30 @@ export interface Committee {
   name: string;
 }
 
+export interface UserCommittees {
+  email: string;
+  name: string;
+  committees: string[];
+}
+
 export const fetchCommittees = async (getAccessTokenSilently: Function) => {
   const accesstoken = await getAccessTokenSilently();
   return fetch(
     (import.meta.env.VITE_BACKEND_URI as string) + "/api/committee/all",
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ` + accesstoken,
+      },
+    }
+  ).then((res) => res.json());
+};
+
+export const fetchUserComittees = async (getAccessTokenSilently: Function): Promise<UserCommittees> => {
+  const accesstoken = await getAccessTokenSilently();
+  return await fetch(
+    (import.meta.env.VITE_BACKEND_URI as string) + "/api/committee/user",
     {
       method: "GET",
       headers: {
