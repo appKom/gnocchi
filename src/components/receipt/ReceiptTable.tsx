496ec0f78a4b68a1a1decaca9f6d02b4992046c2
akselsf <akselsf>
2025-02-07 16:12:22 +0100

    refactor: :zap: Fix auth
    #43

--- src/components/receipt/ReceiptTable.tsx
+++ src/components/receipt/ReceiptTable.tsx
@@ -9,7 +9,7 @@ interface ReceiptTableProps {
   receiptsLoading: boolean;
   onSetActive: () => void;
   onSetHistory: () => void;
-  receiptStatus: String | undefined;
+  receiptStatus: String | null;
 }
 
 const ReceiptTable = ({
