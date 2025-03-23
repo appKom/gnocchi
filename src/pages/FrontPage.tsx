2f226e7e894508250f9704416ad3073b87699b20
johngothe <johngot@stud.ntnu.no>
2025-03-23 14:57:57 +0100

    fix: updated responsiveness for FrontInfo and adjust layout in FrontPage

--- src/pages/FrontPage.tsx
+++ src/pages/FrontPage.tsx
@@ -7,22 +7,19 @@ import happy from "../resources/frontpage/happy.png";
 import useAutobankStore from "../store/autobankstore";
 import { useAuth } from "react-oidc-context";
 export default function frontPage() {
- 
-  const { isAuthenticated, signinRedirect} = useAuth();
-  
+  const { isAuthenticated, signinRedirect } = useAuth();
 
   return (
-    <div className="bg-[#2E6E53] flex flex-col space-y-4">
+    <div className="bg-[#2E6E53] flex flex-col">
       <Topbar />
       <FrontInfo />
       <Bottombar />
-      <div className="z-30">
-        <header className="w-1/2 text-white text-5xl font-bold z-30 px-10">
-        
+      <div className="z-30 mb-8">
+        <header className="w-full lg:w-1/2 text-white text-5xl font-bold z-30 px-10 mb-8">
           Autobank
         </header>
-        <div className="flex flex-row items-center space-x-4 mx-10">
-          <div className="w-1/2">
+        <div className="flex flex-row items-center space-x-10 mx-10 mb-8">
+          <div className="w-full lg:w-1/2">
             <p className="text-white px-20 text-left text-xl">
               Trenger du økonomisk støtte til diverse arrangement? Autobank er
               et hjelpemiddel for studenter i Online som ønsker å søke om
@@ -32,19 +29,22 @@ export default function frontPage() {
               dine saker.
             </p>
           </div>
-          <div className="w-1/2 ">
-            <img src={happy} alt="" className="money-honey px-20" />
+          <div className="hidden lg:block w-1/2">
+            <img
+              src={happy}
+              alt=""
+              className="money-honey px-20 h-80 min-w-full object-contain"
+            />
           </div>
         </div>
-        <div className="flex w-1/2 flex-row items-center justify-center space-x-10">
-          {isAuthenticated && (
+        <div className="flex w-full lg:w-1/2 flex-row items-center justify-center space-x-10">
+          {!isAuthenticated && (
             <Button
               title={"Logg inn ->"}
               color={"darkGreen"}
               onClick={() => signinRedirect()}
             ></Button>
           )}
-
 
           <Button title={"FAQ"} color={"white"} href="/faq"></Button>
           <Button
