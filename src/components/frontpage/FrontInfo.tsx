5d90324fe1d9e67da1defe8b25f61f4bf565f5d4
Dina Marie Stenrud <dinamst@ntnu.no>
2025-09-28 16:13:04 +0200

    style: Endret på forsiden, flyttet hendene lenger ut og endret tekst

--- src/components/frontpage/FrontInfo.tsx
+++ src/components/frontpage/FrontInfo.tsx
@@ -5,21 +5,18 @@ export default function FrontInfo() {
   return (
     <div className=" bg-[#282c34] w-full h-screen">
       <div className="text-center text-white absolute w-[100%] top-[50%] font-bold text-4xl">
-        <span className="block">Det er ikke hvor mye penger du tjener,</span>
-        <span className="block">men hvor mye du beholder.</span>
+        <span className="block">Velkommen til Autobank!</span>
+        <span className="block text-2xl">Søk om støtte eller</span>
+        <span className="block text-2xl">legg inn kvittering for utlegg</span>
+  
       </div>
-      <img
-        src={money1}
-        alt=""
-        className="money-money object-contain  w-[500px] h-auto absolute right-0 overflow-hidden top-[30%] hidden lg:block"
-      />
 
-      <div className="flipped  absolute top-[20%] left-0 hidden lg:block">
-        <img
-          src={money2}
-          alt=""
-          className="money-money object-contain  w-[500px] h-auto left-0"
-        />
+      <img src={money1} alt="" className="money-money object-contain w-[500px] h-auto absolute top-[30%] hidden lg:block
+                  right-[-50px] lg:right-[-50px]"/>
+
+
+      <div className="flipped absolute top-[20%] hidden lg:block left-[-250px] lg:left-[-50px]">
+        <img src={money2} alt="" className="money-money object-contain w-[500px] h-auto " />
       </div>
     </div>
   );
